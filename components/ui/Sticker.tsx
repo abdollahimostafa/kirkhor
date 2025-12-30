4da8ffe95ca23b3e6b78fcd0c8d8a6b2e46b08c0
Mostafa <zxmostafaxz@gmail.com>
2025-12-30 18:51:15 +0330

    add

--- components/ui/Sticker.tsx
+++ components/ui/Sticker.tsx
@@ -1,7 +1,13 @@
 export default function SupportButton() {
   return (
-<a href="tel:09998804646" className="fixed text-xs left-4 top-1/6 -translate-y-1/2 bg-blue-500 text-white px-4 py-2 rounded-t-lg cursor-pointer z-20 shadow-lg  hover:bg-blue-700 transition origin-left rotate-90">
-ارتباط  شرکت ها با سکو</a>
+<a href="tel:09998804646" className=" flex flex-col fixed text-xs left-4 top-1/6 -translate-y-1/2 bg-blue-500 text-white px-4 py-2 rounded-t-lg cursor-pointer z-20 shadow-lg  hover:bg-blue-700 transition origin-left rotate-90">
+<span className="text-center text-[9px] md:text-xs opacity-70">
+    
+    ارتباط  شرکت ها با سکو
+</span> 
+ <span className="ml-2 text-center  ">09998804646</span>
+
+</a>
   );
 }
 
